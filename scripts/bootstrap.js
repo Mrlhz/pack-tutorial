3d67a058536bb8828daa906dde648238299388e8
mrlhz <mr__lhz@163.com>
2020-06-03 22:46:45 +0800

    copy file

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,73 +1,53 @@
 const fs = require('fs')
 const path = require('path')
 
+const defaultRe = /\{\{((?:.|\r?\n)+?)\}\}/g;
+
 const templateDir = path.resolve(__dirname, '../template/card')
-const distDir = path.resolve(__dirname, '../dist')
-const files = fs.readdirSync(templateDir)
+const distDir = path.resolve(__dirname, '../dist/card')
+
+!fs.existsSync(distDir) && fs.mkdirSync(distDir)
 
 
-// files.forEach(file => {
+function copyFiles(from, to) {
+	// check the from && to path exists
 
-// 	console.log(file)
-// })
-
-
-function deepSync(dir, target, outDirs, dirList = [], fileList = [], copyList = []) {
-	fs.readdirSync(dir).forEach(file => {
-		const child = path.join(dir, file)
-		const output = path.join(outDirs, dir.split(target)[1] || '', file)
-		const stat = fs.statSync(child)
-		// console.log('file', dir, dir.split(target))
+	fs.readdirSync(from).forEach(file => {
+		const children = path.join(from, file)
+		const output = path.join(to, file)
+		const stat = fs.statSync(children)
+		const copy = !fs.existsSync(output)
+		// console.log(file, copy)
+		// console.log(children)
+		// console.log(output)
 		if (stat.isDirectory()) {
-			// dirList.push(child)
-			dirList.push(output)
-			deepSync(child, target, outDirs, dirList, fileList, copyList)
-		} else {
-			fileList.push(child)
-			copyList.push(output)
+			copy && fs.mkdirSync(output)
+			copyFiles(children, output)
+		} else if (stat.isFile()) {
+			/* copy && */ fs.copyFileSync(children, output)
 		}
-	})
-}
 
-const dirList = []
-const fileList = []
-const copyList = []
-deepSync(templateDir, 'card', 'D:\\web\\myblog\\manage-cli\\dist\\card', dirList, fileList, copyList)
-console.log(dirList, fileList, copyList)
-
-
-
-function makepSync(dirs = []) {
-	dirs = Array.isArray(dirs) ? dirs : [dirs]
-	dirs.forEach(dir => {
-		let parts = dir.split(path.sep)
-		// console.log(parts)
-		for (let i = 1; i <= parts.length; i++) {
-			let parent = parts.slice(0, i).join(path.sep)
-			// console.log(parent)
-			try {
-				fs.accessSync(parent)
-			} catch (error) {
-				fs.mkdirSync(parent)
-			}
-
-		}
 	})
 
 }
 
-function callback(err) {
-  if (err) throw err
-  console.log('源文件已拷贝到目标文件')
-}
 
-function handleCopyFileSync(copyFileSrc = [], copyFileOutput = []) {
+function replaceMatch(path, matchList = []) {
+	matchList.forEach(match => {
 
-	copyFileSrc.forEach((src, index) => {
-		fs.copyFile(src, copyFileOutput[index], callback)
 	})
 }
 
+function writeJsonFile(path, jsonList = []) {
 
-makepSync(dirList)
-handleCopyFileSync(fileList, copyList)
+}
+
+
+function init() {
+	copyFiles(templateDir, distDir)
+
+
+}
+
+
+init()
